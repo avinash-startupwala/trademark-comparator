ea4f34aed77a3d9947cda7bdee24a0cd676e6a0d
avinash pawar <30456983+avinash-startupwala@users.noreply.github.com>
2018-02-14 23:17:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 
 echo "welcome";
 
-$contents = file_get_contents('http://www.startupwala.in/tm_comparator_api/get_new_trademarks.php');
+$contents = file_get_contents('http://www.startupwala.in/autosuggest/index.php');
 echo $contents;
 ?>
