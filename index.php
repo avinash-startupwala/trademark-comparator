97c6273f8d43ee83ad7f996a635ab52005ff7dfe
avinash pawar <30456983+avinash-startupwala@users.noreply.github.com>
2018-02-15 00:52:44 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,14 @@
 
  $id = $_GET['id'];
 
+$ide = $_GET['ide'];
+
 echo "ID On Heroku: ".$id." ";
 
+echo " IDE on Heroku: ".$ide;
 
-$url = "http://www.startupwala.in/tm_comparator_api/get_new_trademarks.php?id=".$id;
+
+$url = "http://www.startupwala.in/tm_comparator_api/get_new_trademarks.php?id=".$id."&&ide=".$ide;
 
 
 $contents = file_get_contents($url);
