8775d0a312c6cb0b5652014f99d419708baa0e2f
avinash pawar <30456983+avinash-startupwala@users.noreply.github.com>
2018-02-14 23:51:52 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 
 echo "welcome";
 
-$contents = file_get_contents('http://www.startupwala.in/autosuggest/index.php');
+$contents = file_get_contents('http://www.startupwala.in/tm_comparator_api/index.php');
 echo $contents;
 ?>
